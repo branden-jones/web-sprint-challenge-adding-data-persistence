323a0137c02855a6d72aee426ad375d4dab27f3d
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-29 21:46:08 -0500

    get all projects working in model and router

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,10 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+async function getAllProjects (){
+    return db('projects')
+}
+
+module.exports = {
+    getAllProjects
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,8 +1,16 @@
 // build your `/api/projects` router here
 const router = require('express').Router()
+const Pro = require('./model')
 
-router.use((req,res, next) => {
-    console.log(`Projects Router connected`)
+router.get('/', async (req,res,next) => {
+    const list = await Pro.getAllProjects()
+    if (list) {res.status(200).json(list)}
+    else { 
+        next({
+            status: 400,
+            message: 'could find any projects'
+        })
+    }
 })
 
 module.exports = router
